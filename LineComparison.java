2275d262f46e7c8b06eb22744766fe54c5f6e7c8
Afrinfatma <afrinfatma4@gmail.com>
2022-06-30 08:01:06 +0530

    Modified:Distance

--- LineComparison.java
+++ LineComparison.java
@@ -5,22 +5,17 @@ public class LineComparison {
 	public static void main(String[] args){
 		Scanner scan=new Scanner(System.in);
 		DecimalFormat decForm=new DecimalFormat("0.##");
-		double x1=0;
-		double x2=0;
-		double y1=0;
-		double y2=0;
-		double distance=0;
-		System.out.println("please enter x1 coordinates");
-		x1=scan.nextInt();
-		System.out.println("please enter x2 coordinates");
-		x2=scan.nextInt();
-		System.out.println("please enter y1 coordinates");
-		y1=scan.nextInt();
-		System.out.println("please enter y2 coordinates");
-		y2=scan.nextInt();
 		
+		System.out.println("please enter x1 coordinates");
+		double x1=scan.nextDouble();
+		System.out.println("please enter x2 coordinates");
+		double x2=scan.nextDouble();
+		System.out.println("please enter y1 coordinates");
+		double y1=scan.nextDouble();
+		System.out.println("please enter y2 coordinates");
+		double y2=scan.nextDouble();
 		scan.close();
-		distance=Math.sqrt(Math.pow(x2-x1,2)+(Math.pow(y2-y1,2)));
-		System.out.println("The distance between the line" + decForm.format(distance));
+		double distance=Math.sqrt(Math.pow(x2-x1,2)+(Math.pow(y2-y1,2)));
+		System.out.println("The distance between the line:" +decForm.format(distance));
 	}
 }
